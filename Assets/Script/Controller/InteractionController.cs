3e07962581b1361be851857cf98f6537a70704ec
Penguin-God <gkswh4860@naver.com>
2021-07-13 17:46:34 +0900

    Part 3 - 1

---
+++ Assets/Script/Controller/InteractionController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    private void Awake()
+    {
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    private bool interactable = false;
+    void Update()
+    {
+        CheckObject();
+        Set_Corsshair(interactable);
+    }
+
+    private Camera cam;
+    private Vector3 mousePosition;
+    RaycastHit rayHit;
+
+    void CheckObject()
+    {
+        mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+        if (Physics.Raycast(cam.ScreenPointToRay(mousePosition), out rayHit, 100))
+        {
+            interactable = Set_ObjectInteractable(rayHit.transform);
+        }
+        else interactable = false;
+    }
+    bool Set_ObjectInteractable(Transform hitTransform)
+    {
+        bool interactable;
+        if (hitTransform.CompareTag("Interaction")) interactable = true;
+        else interactable = false;
+        return interactable;
+    }
+
+    [SerializeField] GameObject normalCorsshair;
+    [SerializeField] GameObject interactiveCorsshair;
+    void Set_Corsshair(bool interactable)
+    {
+        interactiveCorsshair.SetActive(interactable);
+        normalCorsshair.SetActive(!interactable);
+    }
+}
