9244bee8f8cb2dbe6e97906f6d5a616ca1c6ae90
Penguin-God <gkswh4860@naver.com>
2021-07-15 15:09:37 +0900

    해상도 고정

---
+++ Assets/Script/FixedResolution.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedResolution : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(1080, 720, true);
+    }
+}
