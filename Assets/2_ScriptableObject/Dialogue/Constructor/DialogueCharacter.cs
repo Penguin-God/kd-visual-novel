db7dbda714c7d591e317cfbfd10a185c042a5bf2
Penguin-God <gkswh4860@naver.com>
2022-05-14 16:51:52 +0900

    폴더 이름 변경, 파일 삭제

--- Assets/2_ScriptableObject/Dialogue/Constructor/DialogueCharacter.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "new Dialogue CharacterObject", menuName = "Scriptable Object / Dialogue / Dialogue CharacterObject")]
-public class DialogueCharacter : DialogueObject
-{
-    
-}
